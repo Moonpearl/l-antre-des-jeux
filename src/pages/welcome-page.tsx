72f4f4f5e59e5178d3a723b9a0b08d574804f8b8
Caro-L-dev <liardcar@gmail.com>
2021-02-22 10:11:36 +0100

    Add jsx content on welcome page

--- src/pages/welcome-page.tsx
+++ src/pages/welcome-page.tsx
@@ -1,26 +1,82 @@
-import * as React from 'react';
-import { Link } from 'gatsby';
+import * as React from 'react'
+import { Link } from 'gatsby'
 
-import Page from '../components/Page';
-import Container from '../components/Container';
-import IndexLayout from '../layouts';
+import Page from '../components/Page'
+import Container from '../components/Container'
+import IndexLayout from '../layouts'
 
 const welcomePage = () => (
   <IndexLayout>
     <Page>
       <Container>
-        <h1>Hi from the welcome page</h1>
+        {/* UNDERLAY BG */}
         <ul>
           <li>
-            <Link to="/a-markdown-page/">Show me some Markdown!</Link>
+            <div>Img1</div>
           </li>
           <li>
-            <Link to="/">Take me back home.</Link>
+            <div>Img2</div>
           </li>
+          <li>
+            <div>Img3</div>
+          </li>
+          <li>
+            <div>Img4</div>
+          </li>
+          <li>
+            <div>Img5</div>
+          </li>
+          <li>
+            <div>Img6</div>
+          </li>
+          <li>
+            <div>Img7</div>
+          </li>
+          <li>
+            <div>Img8</div>
+          </li>
+        </ul>
+
+        {/* OVERLAY */}
+        <ul>
+          <li>
+            <div>Jeux de société</div>
+          </li>
+          <li>
+            <div>Jeux d'enfants</div>
+          </li>
+          <li>
+            <div>Jeux de figurines</div>
+          </li>
+          <li>
+            <div>Jeux de roles</div>
+          </li>
+          <li>
+            <div>Jeux d'histoires</div>
+          </li>
+          <li>
+            <div>Jeux de cartes</div>
+          </li>
+          <li>
+            <div>Jeux classiques</div>
+          </li>
+          <li>
+            <div>Accessoires</div>
+          </li>
+        </ul>
+
+        <ul>
+          {/* FOOTER */}
+          <div className="footer">
+            <p>Copyright 2021 - L'Antre des jeux</p>
+            <li>
+              <Link to="/">Take me back home.</Link>
+            </li>
+          </div>
         </ul>
       </Container>
     </Page>
   </IndexLayout>
 )
 
-export default welcomePage;
+export default welcomePage
