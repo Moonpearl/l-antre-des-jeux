15deeb80100f2b97f083820d3a9751a3b4d6f0b6
Caro-L-dev <liardcar@gmail.com>
2021-02-25 13:47:20 +0100

    Shelf page - add content

--- src/pages/shelf.tsx
+++ src/pages/shelf.tsx
@@ -9,10 +9,33 @@ const ShelfPage: FC = () => (
   <IndexLayout>
     <Page>
       <Container>
-        <h1>Nom du rayon</h1>
-        <p>
-          1 <Link to="/">Page 1</Link>
-        </p>
+        <div className="ShelfContainerIconsHome">
+          <div>Icone home</div>
+          <div>Icone home2</div>
+        </div>
+
+        <div className="LeftSide">
+          <p>Nombre de produits dans ce rayon: 150</p>
+          <div>Barre multicritères ou image</div>
+        </div>
+
+        <div className="RightSide--ShelfContainer">
+          <h1>Nom du rayon</h1>
+          <div className="ProductContainer">
+            <img className="ProductImg"></img>
+            <h2>Titre du produit</h2>
+            <div>Panier</div>
+          </div>
+          <ul className="ShelfPages">
+            <ul>arrow</ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+            <li>...</li>
+            <ul>arrow</ul>
+          </ul>
+        </div>
+        <div>Composant barre de recherche</div>
       </Container>
     </Page>
   </IndexLayout>
