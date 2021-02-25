ca98b3dcddca16bf4fe812542c1d9ffde613c457
Caro-L-dev <liardcar@gmail.com>
2021-02-25 14:52:43 +0100

    Shelf page - styles passed on Emotion

--- src/pages/shelf.tsx
+++ src/pages/shelf.tsx
@@ -1,43 +1,109 @@
 import React, { FC } from 'react';
+import styled from '@emotion/styled';
 import { Link } from 'gatsby';
-
-import Page from '../components/Page';
-import Container from '../components/Container';
 import IndexLayout from '../layouts';
+import { FaHome, FaChevronLeft, FaChevronRight, FaSearch } from 'react-icons/fa';
+
+// EMOTION COMPONENTS ------------------------------------------------ //
+
+const BgColorPage = styled.div`
+  background-color: #ece0d4;
+`;
+
+const IconsNavigationTopLeft = styled.div`
+  background-color: pink;
+`;
+
+const LeftSide = styled.div`
+  background-color: orange;
+  display: flex;
+  flex-direction: column;
+`;
+
+const LeftSideProductsNumber = styled.p`
+  background-color: #ffde59;
+`;
+
+const LeftSideMultiSidebar = styled.div`
+  background-color: #03989e;
+`;
+
+const RightSideShelfContainer = styled.div`
+  background-color: grey;
+`;
+
+const RightSideShelfTitle = styled.h2`
+  background-color: #03989e;
+  color: #ffde59;
+`;
+
+const ProductContainer = styled.div`
+  background-color: white;
+`;
+
+const ProductImg = styled.img`
+  background-color: yellow;
+`;
+
+const ProductTitle = styled.h3`
+  background-color: green;
+`;
+
+const ProductShoppingBasket = styled.div`
+  background-color: pink;
+`;
+
+const ShelfPagesButtons = styled.ul`
+  background-color: greenyellow;
+`;
+
+const ShelfPagesButtonsNumber = styled.li`
+  background-color: white;
+`;
+
+const SearchBar = styled.div`
+  background-color: white;
+`;
+
+// SHELF PAGE ----------------------------------------------------- //
 
 const ShelfPage: FC = () => (
   <IndexLayout>
-    <Page>
-      <Container>
-        <div className="ShelfContainerIconsHome">
-          <div>Icone home</div>
-          <div>Icone home2</div>
-        </div>
+    <BgColorPage>
+      <IconsNavigationTopLeft>
+        <FaHome />
+        <FaChevronLeft />
+      </IconsNavigationTopLeft>
 
-        <div className="LeftSide">
-          <p>Nombre de produits dans ce rayon: 150</p>
-          <div>Barre multicritères ou image</div>
-        </div>
+      <LeftSide>
+        <LeftSideProductsNumber>Nombre de produits dans ce rayon: 150</LeftSideProductsNumber>
+        <LeftSideMultiSidebar> Barre multicritères ou image</LeftSideMultiSidebar>
+      </LeftSide>
 
-        <div className="RightSide--ShelfContainer">
-          <h1>Nom du rayon</h1>
-          <div className="ProductContainer">
-            <img className="ProductImg"></img>
-            <h2>Titre du produit</h2>
-            <div>Panier</div>
-          </div>
-          <ul className="ShelfPages">
-            <ul>arrow</ul>
-            <li>1</li>
-            <li>2</li>
-            <li>3</li>
-            <li>...</li>
-            <ul>arrow</ul>
-          </ul>
-        </div>
-        <div>Composant barre de recherche</div>
-      </Container>
-    </Page>
+      <RightSideShelfContainer>
+        <RightSideShelfTitle>Nom du rayon</RightSideShelfTitle>
+
+        <ProductContainer>
+          <ProductImg />
+          <ProductTitle>Titre du produit</ProductTitle>
+          <ProductShoppingBasket>Panier</ProductShoppingBasket>
+        </ProductContainer>
+
+        <ShelfPagesButtons>
+          <FaChevronLeft />
+          <ShelfPagesButtonsNumber>1</ShelfPagesButtonsNumber>
+          <ShelfPagesButtonsNumber>2</ShelfPagesButtonsNumber>
+          <ShelfPagesButtonsNumber>3</ShelfPagesButtonsNumber>
+          <ShelfPagesButtonsNumber>...</ShelfPagesButtonsNumber>
+
+          <FaChevronRight />
+        </ShelfPagesButtons>
+      </RightSideShelfContainer>
+
+      <SearchBar>
+        <FaSearch />
+      </SearchBar>
+    </BgColorPage>
   </IndexLayout>
 );
 
