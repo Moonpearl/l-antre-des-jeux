05dd3c327f669fdb72ab2015d11c577deabac941
Romain Tello <romain.tello@gmail.com>
2021-03-03 15:01:58 +0100

    Beautify buttons

--- src/components/styles/index.ts
+++ src/components/styles/index.ts
@@ -1,5 +1,6 @@
 export { default as BackgroundColorContainer } from './background-color-container';
 export { default as BackgroundImageContainer } from './background-image-container';
+export { default as Button } from './button';
 export { default as Filler } from './filler';
 export { default as FlexContainer } from './flex-container';
 export { default as MainContainer } from './main-container';
---
+++ src/templates/product.tsx
@@ -0,0 +1,3 @@
+const ProductPage = () => <div />;
+
+export default ProductPage;
