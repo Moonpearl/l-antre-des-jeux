188878cf09469580cf1b14ed751235c2edad7723
Caro-L-dev <liardcar@gmail.com>
2021-03-03 10:48:48 +0100

    Improve selections layout

--- src/components/selection.tsx
+++ src/components/selection.tsx
@@ -1,175 +1,130 @@
 import React, { FC } from 'react';
 import styled from '@emotion/styled';
 import { GraphcmsSelection } from '../models/graphcms/assets';
-import { FaShoppingBasket, FaBookmark } from 'react-icons/fa';
+import { FaBookmark } from 'react-icons/fa';
+import { MainContainer } from './styles';
 import '../models/graphcms/assets/shelf';
 
-interface SelectionProps {
-  selection: GraphcmsSelection;
-}
-
-const SelectionContainer = styled.div`
-  /*  background-color: pink;*/
-  display: flex;
-  flex-direction: column;
-  flex-wrap: nowrap;
-  position: relative;
-
-  margin: 2em;
-  border-radius: 2em;
-  box-sizing: border-box;
-  overflow: hidden;
-
-  @media only screen and (min-width: 268px) (max-width: 600px) {
-    max-width: 30em;
-    margin: 0 auto;
-  }
-`;
-
 const StyledSelection = styled.div`
-  /* background-color: blue;*/
+  position: relative;
   display: flex;
   flex-direction: column;
   flex-wrap: nowrap;
-
-  @media only screen and (min-width: 268px) (max-width: 600px) {
-    max-width: 30em;
-    margin: 0 auto;
-  }
+  padding: 2rem;
+  border: 4px solid teal;
+  border-radius: 1em;
+  margin-bottom: 4rem;
 `;
 
-const TitleSelection = styled.h3`
-  /* background-color: yellow;*/
+const SelectionTitle = styled.h3`
+  background-color: white;
+  position: absolute;
+  top: -1.5rem;
+  text-align: center;
+  font-family: 'Oswald', sans-serif;
+  letter-spacing: 0.05em;
   color: teal;
   font-weight: bold;
   text-transform: uppercase;
   font-style: italic;
   font-size: 2em;
-  margin: 0 auto;
-  margin-bottom: 0.2em;
+  margin: 0;
+  padding: 0 0.5em;
 `;
 
-const BoardGamesSelection = styled.ol`
-  /* background-color: goldenrod;*/
+const ProductList = styled.ol`
   position: relative;
-  justify-content: center;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(10rem, 1fr));
+  grid-auto-rows: 10rem;
+  gap: 2rem;
   border-radius: 2em;
-
-  display: flex;
-  flex-direction: row;
-  flex-wrap: wrap;
 `;
 
 const Product = styled.div`
-  /* margin: 2em;*/
-  padding: 1em;
-  font-style: italic;
-  max-height: 18em;
-  max-width: 22em;
   position: relative;
-`;
+  height: 100%;
 
-const BoardGamesNameSelection = styled.li`
-  background-color: #4caf50;
-  color: white;
-  font-size: 16px;
-  padding: 16px 32px;
-`;
-
-const BoardGamesImgSelection = styled.img`
-  /* background-color: blue;*/
-  /* justify-content: center;*/
-  position: relative;
-  /* margin: 0 auto;*/
-
-  max-height: 15em;
-  bottom: 70px;
-  opacity: 1;
-  width: 100%;
-
-  transition: 0.5s ease;
-  backface-visibility: hidden;
-
-  max-width: 30em;
-  margin: 0 auto;
-
-  &:hover {
-    opacity: 0.2;
-  }
-`;
-
-const Middle = styled.div`
-  transition: 0.5s ease;
-  opacity: 0;
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  -ms-transform: translate(-50%, -50%);
-  text-align: center;
-
-  &:hover {
+  &:hover *:first-child {
     opacity: 1;
   }
 `;
 
-const Button = styled.button`
-  background-color: #0093e9;
-  background-image: linear-gradient(160deg, #0093e9 0%, #80d0c7 100%);
-
-  border-radius: 2em;
+const ProductName = styled.li`
   color: white;
-  font-weight: bold;
-  display: inline - block;
-  cursor: pointer;
-  font-size: 1em;
-  padding: 10px 20px;
-  text-shadow: 0px 3px 0px #0093e9;
-  box-shadow: inset 0px 1px 0px 0px #d9fbbe;
-  border: none;
-  font-family: 'Ubuntu Condensed', 'sans - serif';
-  font-style: italic;
-  letter-spacing: 0.2em;
-  line-height: 0.7em;
-
-  margin: 1em auto;
-  padding: 20px;
 `;
 
-const ProductContainerItem = styled.div`
-  /*background-color: palegreen;*/
-  position: relative;
-  font-size: 2em;
-  margin-left: 7.5em;
-  /*  bottom: 1.3em;*/
+const ProductImage = styled.img`
+  height: 100%;
+  margin: 0 auto;
+  display: block;
+  object-fit: scale-down;
+  border-radius: 0.5em;
+`;
+
+const Overlay = styled.div`
+  opacity: 0;
+  background-color: rgba(0, 0, 0, 0.5);
+  transition: 0.5s ease;
+  position: absolute;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  z-index: 1;
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  border-radius: 0.5em;
+`;
+
+const ProductShelf = styled.div`
+  position: absolute;
+  top: -0.5rem;
+  right: 0;
   z-index: 6;
-  color: palegreen;
+  font-size: 3em;
+  filter: drop-shadow(4px 4px 2px rgba(96, 96, 96, 0.5));
 `;
+
+const SelectionDescription = styled.div`
+  text-align: justify;
+  margin-bottom: 1em;
+`;
+
+interface SelectionProps {
+  selection: GraphcmsSelection;
+}
 
 const Selection: FC<SelectionProps> = ({ selection }) => (
-  <SelectionContainer>
+  <MainContainer>
     <StyledSelection>
-      <TitleSelection>{selection.name}</TitleSelection>
+      <SelectionTitle>{selection.name}</SelectionTitle>
+      <SelectionDescription>{selection.description}</SelectionDescription>
 
-      <BoardGamesSelection>
+      <ProductList>
         {selection.products.map(product => (
-          <>
+          <li key={product.slug}>
             <Product>
-              <ProductContainerItem>
-                <FaBookmark size="70" color={product.shelf} />
-              </ProductContainerItem>
+              <Overlay>
+                <ProductName>{product.name}</ProductName>
+                <button>Voir</button>
+              </Overlay>
 
-              <BoardGamesImgSelection src={product.imageUrl} alt="bg"></BoardGamesImgSelection>
-              <Middle>
-                <BoardGamesNameSelection key={product.slug}>{product.name}</BoardGamesNameSelection>
-              </Middle>
+              {product.shelf && (
+                <ProductShelf>
+                  <FaBookmark color={product.shelf.backgroundColor.css} />
+                </ProductShelf>
+              )}
+
+              <ProductImage src={product.imageUrl} alt={product.name} />
             </Product>
-          </>
+          </li>
         ))}
-      </BoardGamesSelection>
-      <Button>En voir plus</Button>
+      </ProductList>
     </StyledSelection>
-  </SelectionContainer>
+  </MainContainer>
 );
 
 export default Selection;
