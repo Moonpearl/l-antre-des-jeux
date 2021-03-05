533c2eaa868901ed3a416d4a5929cd494f598abe
Romain Tello <romain.tello@gmail.com>
2021-03-05 14:28:27 +0100

    Fix seams between fillers

--- src/components/styles/filler.tsx
+++ src/components/styles/filler.tsx
@@ -10,6 +10,7 @@ const Filler = styled.div<FillerProps>`
   background-color: ${({ color }): string => color || 'rgba(0, 0, 0, 0)'};
   min-height: ${({ height }): string => height};
   ${({ marginTop }): string => (marginTop ? `margin-top: ${marginTop};` : '')}
+  transform: scale(1, 1.1);
 `;
 
 export default Filler;
