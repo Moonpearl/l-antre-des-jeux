5652e70c3224f16cdf259049e55359695cd38716
Caro-L-dev <liardcar@gmail.com>
2021-03-15 10:40:07 +0100

    NotFoundPage styles

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -4,18 +4,78 @@ import { Link } from 'gatsby';
 import Page from '../components/Page';
 import Container from '../components/Container';
 import IndexLayout from '../layouts';
+import styled from '@emotion/styled';
+
+
+const Bg = styled.div`
+background-color: #154360;
+`;
+
+const ContainerText = styled.div`
+justify-content: center;
+vertical-align: center;
+margin: 15em auto;
+color: white;
+
+`;
+
+const Title = styled.h1`
+text-align: center;
+font-weight: bold;
+font-size: 8em;
+color: #2980B9;
+margin-bottom: 0.2em;
+font-family: 'Oswald', sans-serif;
+
+
+`;
+
+const Text = styled.p`
+text-align: center;
+letter-spacing: 0.2em;
+font-size: 2em;
+font-family: 'Oswald', sans-serif;
+`;
+
+const LinkStyled = styled.p`
+color: #2980B9;
+font-weight: bold;
+font-style: italic;
+letter-spacing: 0.2em;
+margin-top: 2em;
+border: 1px solid;
+padding: 1em;
+transition: background-color 0.3s ease;
+font-family: 'Oswald', sans-serif;
+border-radius: 2em;
+
+&:hover{
+  background-color: #2980B9;
+  color: white;
+}
+`;
+
+
 
 const NotFoundPage: FC = () => (
   <IndexLayout>
-    <Page>
-      <Container>
-        <h1>404: Page not found.</h1>
-        <p>
-          You&apos;ve hit the void. <Link to="/">Go back.</Link>
-        </p>
-      </Container>
-    </Page>
-  </IndexLayout>
+    <Bg>
+      <Page>
+
+        <Container>
+
+          <ContainerText>
+            <Title>Oups !</Title>
+            <Text>Cette page n'existe pas.</Text>
+            <Text>Veuillez relancer votre dé et ...</Text>
+            <Text> <LinkStyled><Link to="/">Revenir à la case départ</Link></LinkStyled></Text>
+          </ContainerText>
+
+        </Container>
+
+      </Page>
+    </Bg>
+  </IndexLayout >
 );
 
 export default NotFoundPage;
