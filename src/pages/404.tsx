af15c02d19f8969c934c24405d05ef2efa4d1ae7
Caro-L-dev <liardcar@gmail.com>
2021-03-17 15:27:12 +0100

    Seo 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -5,17 +5,24 @@ import Page from '../components/Page';
 import Container from '../components/Container';
 import IndexLayout from '../layouts';
 
-const NotFoundPage: FC = () => (
-  <IndexLayout title="404" description="Page non trouvé" keywords="404, notFound" openGraphImage="" >
-    <Page>
-      <Container>
-        <h1>404: Page not found.</h1>
-        <p>
-          You&apos;ve hit the void. <Link to="/">Go back.</Link>
-        </p>
-      </Container>
-    </Page>
-  </IndexLayout>
-);
+const NotFoundPage: FC = () => {
+  const { graphCmsPages: pages } = data;
+
+  return (
+    <IndexLayout
+      title={pages?.title}
+      description={pages?.description}
+      openGraphImage={pages?.openGraphImage}  >
+      <Page>
+        <Container>
+          <h1>404: Page not found.</h1>
+          <p>
+            You&apos;ve hit the void. <Link to="/">Go back.</Link>
+          </p>
+        </Container>
+      </Page>
+    </IndexLayout>
+  );
+};
 
 export default NotFoundPage;
