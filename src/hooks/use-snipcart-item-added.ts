959adc6a439c03ab8617fa8063bb08c16e276e37
Romain Tello <romain.tello@gmail.com>
2021-05-01 18:57:02 +0200

    Fix window not available in server-side rendering

--- src/hooks/use-snipcart-item-added.ts
+++ src/hooks/use-snipcart-item-added.ts
@@ -2,24 +2,9 @@ import { useEffect } from 'react';
 import { SnipcartProductItem } from '../models/snipcart';
 
 export default (callback: (data: SnipcartProductItem) => void): void => {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  let _window: any;
-
-  if (typeof window !== `undefined`) {
-    _window = window;
-  } else {
-    _window = {
-      Snipcart: {
-        events: {
-          on: (): void => undefined,
-        },
-      },
-    };
-  }
-
-  const { Snipcart } = _window;
-
   useEffect(() => {
+    const { Snipcart } = window;
+    if (!Snipcart) return;
     return Snipcart.events.on('item.adding', (data: SnipcartProductItem) => {
       callback(data);
     });
