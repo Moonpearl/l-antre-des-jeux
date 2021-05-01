d4617f8dbfd2d91c8777bd889fa0cd032ac041c2
Romain Tello <romain.tello@gmail.com>
2021-05-01 19:43:51 +0200

    Remove styles

--- src/components/snipcart-controller.tsx
+++ src/components/snipcart-controller.tsx
@@ -8,59 +8,59 @@ import { SnipcartProductItem } from '../models/snipcart';
 import { Button } from './styles';
 
 // SECTION Styles
-const styles = {
-  Container: styled.div`
-    position: fixed;
-    top: 0;
-    left: 0;
-    right: 0;
-    bottom: 0;
-    background-color: rgba(0, 0, 0, 0.5);
-    z-index: 10000;
-    display: flex;
-    flex-flow: column nowrap;
-    justify-content: center;
-    align-items: center;
-    transition: opacity 0.3s ease;
-    &.hidden {
-      opacity: 0;
-      pointer-events: none;
-    }
-  `,
-  Modal: styled.div`
-    max-width: 480px;
-    background-color: white;
-    padding: 1em;
-    border-radius: 1em;
-    display: flex;
-    flex-flow: column nowrap;
-    align-items: center;
-    gap: 1em;
-    text-align: justify;
-    -webkit-box-shadow: 0px 10px 25px -7px rgba(0, 0, 0, 0.5), 5px 5px 15px 5px rgba(0, 0, 0, 0);
-    box-shadow: 0px 10px 25px -7px rgba(0, 0, 0, 0.5), 5px 5px 15px 5px rgba(0, 0, 0, 0);
-  `,
-  FlexContainer: styled.div`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    gap: 1em;
-  `,
-  ProductImage: styled.img`
-    width: 4em;
-  `,
-  Alert: styled.div`
-    background-color: #fff0c0;
-    border: 1px solid #d8a601;
-    color: 1px solid #503d00;
-    border-radius: 0.25em;
-    font-size: 0.9em;
-    padding: 0.5em 1em;
-    & > strong {
-      font-weight: bold;
-    }
-  `,
-};
+// const styles = {
+//   Container: styled.div`
+//     position: fixed;
+//     top: 0;
+//     left: 0;
+//     right: 0;
+//     bottom: 0;
+//     background-color: rgba(0, 0, 0, 0.5);
+//     z-index: 10000;
+//     display: flex;
+//     flex-flow: column nowrap;
+//     justify-content: center;
+//     align-items: center;
+//     transition: opacity 0.3s ease;
+//     &.hidden {
+//       opacity: 0;
+//       pointer-events: none;
+//     }
+//   `,
+//   Modal: styled.div`
+//     max-width: 480px;
+//     background-color: white;
+//     padding: 1em;
+//     border-radius: 1em;
+//     display: flex;
+//     flex-flow: column nowrap;
+//     align-items: center;
+//     gap: 1em;
+//     text-align: justify;
+//     -webkit-box-shadow: 0px 10px 25px -7px rgba(0, 0, 0, 0.5), 5px 5px 15px 5px rgba(0, 0, 0, 0);
+//     box-shadow: 0px 10px 25px -7px rgba(0, 0, 0, 0.5), 5px 5px 15px 5px rgba(0, 0, 0, 0);
+//   `,
+//   FlexContainer: styled.div`
+//     display: flex;
+//     justify-content: center;
+//     align-items: center;
+//     gap: 1em;
+//   `,
+//   ProductImage: styled.img`
+//     width: 4em;
+//   `,
+//   Alert: styled.div`
+//     background-color: #fff0c0;
+//     border: 1px solid #d8a601;
+//     color: 1px solid #503d00;
+//     border-radius: 0.25em;
+//     font-size: 0.9em;
+//     padding: 0.5em 1em;
+//     & > strong {
+//       font-weight: bold;
+//     }
+//   `,
+// };
 // !SECTION
 
 // SECTION Main component
